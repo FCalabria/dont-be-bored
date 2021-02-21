996a3e1cf8ddfc259c61439d5b5adc527cb0fde9
Paqui <paqui.calabria@adevinta.com>
2021-02-21 14:05:56 +0100

    Add emoji depending on activity type

--- src/Idea.svelte
+++ src/Idea.svelte
@@ -1,7 +1,22 @@
 <script>
     export let idea = {}
+    const typesMap = {
+        education: '🤓',
+        recreational: '💃',
+        social: '🥳',
+        diy: '🧑‍🔧',
+        charity: '😇',
+        cooking: '🧑‍🍳',
+        relaxation: '💆',
+        music: '🧑‍🎤',
+        busywork: '🧑‍💼'
+    }
+    $: emoji = typesMap[idea.type] || '😀'
 </script>
-<div class="m-2 p-2">
-    <!-- Show emojis depending on type -->
-    {idea.activity}
+<div class="m-2 px-2 py-6 border-2 border-amber-600 bg-gray-50 flex">
+    <div class="w-1/6 flex items-center justify-center text-3xl">{emoji}</div>
+    <div class="flex-grow text-left">
+        <h2 class="text-lg font-semibold text-amber-800 border-b-2 border-amber-600 pb-4 mb-4">{idea.activity}</h2>
+        <p><span class="font-light text-gray-600">People needed:</span> {idea.participants}</p>
+    </div>
 </div>
